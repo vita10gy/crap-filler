f321dfb7641041c8caee21b7ef43d88c122ba7bf
vita10gy <vita10gy@charter.net>
2016-02-27 15:41:00 -0500

    It actually does something, barely

--- src/fill/filler.js
+++ src/fill/filler.js
@@ -1,7 +1,16 @@
 console.log('filler');
-var CrapFiller = function() {
-    $('input:enabled:not([readonly])').each(function () {
-      console.log($(this).attr("name"));
-    });
+
+
+
+
+$(':input:enabled:not([readonly]):not([type="submit"]):not([type="hidden"])').each(function () {
+  console.log($(this).attr("name"));
+  
+  if($(this).attr("type")=="email"){
+    $(this).val("joe"+ (new Date()/1) + "@somenotrealcompany.com");
   }
+  
+  
+});
+
 
